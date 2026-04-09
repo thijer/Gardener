abb57c5a223a2fdc445eb211cdcc17d6e6af0378
Thije Robbe <thijer@hotmail.nl>
2026-04-09 12:18:23 +0200

    moved DebugStreamer functionality to Debug.

--- Gardener-main/Debug/Debug.hpp
+++ Gardener-main/Debug/Debug.hpp
@@ -4,70 +4,63 @@
 #include <vector>
 #include "Stream.h"
 
-class DebugStreamer
-{
-    public:
-        DebugStreamer(Stream* p): streamer(p) { }
-
-        template<typename T, typename... Args>
-        void print(T t, Args... args)
-        {
-            streamer->print(t);
-            print(args...);
-        }
-
-        void print()
-        {
-            streamer->println();
-        }
-        int available()
-        {
-            return streamer->available();
-        }
-        
-        size_t write(const uint8_t* buffer, size_t size)
-        {
-            return streamer->write(buffer, size);   
-        }
-
-
-    private:
-        Stream* streamer;
-};
-
 class Debug : public Print
 {
     public:
-        Debug(std::initializer_list<Stream*> pr)
-        {
-            streamers.reserve(pr.size());
-            for(Stream* p : pr)
-            {
-                streamers.push_back(DebugStreamer(p));
-            }
-        }
+        Debug(std::initializer_list<Stream*> s):
+            streamers(s)
+        {}
 
+        /// @brief Print a variable number of values/strings to all connected `Stream`ers.
+        /// @tparam ...Args Types of the values.
+        /// @param ...args The values.
         template<typename... Args>
         void print(Args... args)
         {
-            for(DebugStreamer p : streamers) p.print(args...);
+            for(Stream* s : streamers) print_to_stream(s, args...);
         }
 
-        
+        /// @brief function used by `print(Args... args)` to print `args` to a `Stream`.
+        /// @tparam T Type to be printed
+        /// @tparam ...Args remaining types
+        /// @param s The `Stream` to print to
+        /// @param t The value to print
+        /// @param ...args remaining values to print
+        template<typename T, typename... Args>
+        void print_to_stream(Stream* s, T t, Args... args)
+        {
+            s->print(t);
+            print_to_stream(s, args...);
+        }
+
+        /// @brief Terminating function of the recursive variadic function.
+        /// @param s The `Stream` to print the terminating character (LF) to.
+        void print_to_stream(Stream* s)
+        {
+            s->print('\n');
+        }
+
+        /// @brief Write bytes to all connected `Stream`ers.
+        /// @param buffer Pointer to a byte array.
+        /// @param size Number of bytes to be written.
+        /// @return Number of bytes written. With multiple `Streams`, this returns the lowest value.
         size_t write(const uint8_t* buffer, size_t size)
         {
             size_t written = SIZE_MAX;
-            for(DebugStreamer p : streamers) written = min(written, p.write(buffer, size));
+            for(Stream* s : streamers) written = min(written, s->write(buffer, size));
             return written;
         }
 
+        /// @brief Write a single byte.
+        /// @param b byte
+        /// @return Number of bytes written.
         size_t write(uint8_t b)
         {
             return write(&b, 1);
         }
 
     private:
-        std::vector<DebugStreamer> streamers;
+        std::vector<Stream*> streamers;
 };
 
 Debug emptydebug({});
